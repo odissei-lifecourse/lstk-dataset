e42ae0b8503253be160d449a5e068651aa8bd5ee
Flavio Hafner <hafner.flavio@gmail.com>
2025-02-07 15:49:54 +0100

    omit non-aggregated files

--- get_metadata.py
+++ get_metadata.py
@@ -1,14 +1,19 @@
 import pathlib
+import re
 from pathlib import Path
 import pyarrow.parquet as pq
 import yaml
 from omegaconf import OmegaConf
+from omegaconf.dictconfig import DictConfig
 
 
-def get_metadata(src_path: pathlib.Path, meta_path: pathlib.Path, exclude: list | None = None) -> None:
+def get_metadata(src_path: pathlib.Path, meta_path: pathlib.Path, config: DictConfig) -> None:
     """Iterate over files in `src_path` and store metadata in `meta_path`."""
+    aggregate_suffix = config.aggregate.suffix
+    names_replace = config.export.replace_suffix
+    extension = config.output.extension
     for file in src_path.glob("*.parquet"):
-        skip = [x in str(file) for x in exclude]
+        skip = [f"{x}{extension}" in str(file) for x in names_replace]
         if any(skip):
             continue
 
@@ -16,7 +21,8 @@ def get_metadata(src_path: pathlib.Path, meta_path: pathlib.Path, exclude: list 
 
         schema_dict = {name: str(field.type) for name, field in zip(table_schema.names, table_schema, strict=False)}
 
-        outfile = meta_path / Path(file.stem).with_suffix(".yaml")
+        file_stem = re.sub(aggregate_suffix, "", file.stem)
+        outfile = meta_path / Path(file_stem).with_suffix(".yaml")
 
         outfile.write_text(yaml.dump(schema_dict, default_flow_style=False))
 
@@ -27,7 +33,7 @@ def main():
     src_path = Path(config.output.path)
     meta_path = Path(config.export.meta_path)
     meta_path.mkdir(parents=True, exist_ok=True)
-    get_metadata(src_path, meta_path, exclude=config.export.exclude)
+    get_metadata(src_path, meta_path, config)
 
 
 if __name__ == "__main__":
