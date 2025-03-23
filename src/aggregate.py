c1e491b9b64e3ca512971037434e91ad7a6b99af
Flavio Hafner <hafner.flavio@gmail.com>
2025-03-23 21:10:40 +0100

    add country of first affil (fixes #3)
    also store final output in new dir

--- src/aggregate.py
+++ src/aggregate.py
@@ -2,11 +2,27 @@ import argparse
 import os
 from pathlib import Path
 import duckdb
+import pyarrow.dataset as ds
 import pyarrow.parquet as pq
 from omegaconf import OmegaConf
+from omegaconf.dictconfig import DictConfig
+from pyarrow.lib import RecordBatchReader
 from tqdm import tqdm
 
 DEVRUN_MAX_CHUNKS = 6
+
+
+def write_batches(stream: RecordBatchReader, destination: str):
+    """Write a RecordBatchReader to parquet."""
+    devrun = os.getenv("DEVRUN", "False") == "True"
+
+    first_batch = next(stream)
+    with pq.ParquetWriter(destination, first_batch.schema) as writer:
+        writer.write_batch(first_batch)
+        for i, batch in tqdm(enumerate(stream), desc="Processing batches"):
+            writer.write_batch(batch)
+            if devrun and i > DEVRUN_MAX_CHUNKS:
+                break
 
 
 def aggregate(query: str, tbl_src: str, result_dir: Path, result_name_suffix: str, chunk_size: int = 100_000) -> None:
@@ -23,26 +39,40 @@ def aggregate(query: str, tbl_src: str, result_dir: Path, result_name_suffix: st
     If the source file is `/path/file.parquet`, the destination is `/path/file_result_name_suffix.parquet`.
 
     """
-    devrun = os.getenv("DEVRUN", "False") == "True"
-
     query = f"{query} FROM '{tbl_src!s}' ORDER BY Year, AuthorId"  # ordering seems to speed up queries 5x
     destination = f"{tbl_src.stem}{result_name_suffix}"
     destination = result_dir / Path(destination).with_suffix(tbl_src.suffix)
 
     with duckdb.connect() as con:
         arrow_stream = con.execute(query).fetch_record_batch(rows_per_batch=chunk_size)
+        write_batches(arrow_stream, destination)
 
-        first_batch = next(arrow_stream)
-        with pq.ParquetWriter(destination, first_batch.schema) as writer:
-            writer.write_batch(first_batch)
-            for i, batch in tqdm(enumerate(arrow_stream), desc="Processing batches"):
-                writer.write_batch(batch)
-                if devrun and i > DEVRUN_MAX_CHUNKS:
-                    break
+
+def postprocess(task_config: DictConfig, output_config: DictConfig, chunk_size: int = 50_000):
+    """Postprocess tables.
+
+    Args:
+        task_config (DictConfig): specifies input tables, query and name of the output.
+    (through output.suffix.preprocess and reference_table.
+        output_config (DictConfig): defines extension, path, and suffix.postprocess.
+        chunk_size (int): Number of rows to process per batch.
+    """
+    data_dict = {}
+    for tbl_name, file_name in task_config.inputs.items():
+        file_path = (output_config.path / Path(file_name)).with_suffix(output_config.extension)
+        data_dict[tbl_name] = ds.dataset(file_path).to_table()
+
+    output_name = Path(f"{task_config.reference_table}{output_config.suffix.postprocess}")
+    destination = (output_config.path / output_name).with_suffix(output_config.extension)
+    with duckdb.connect() as con:
+        for tbl_name, tbl in data_dict.items():
+            con.register(tbl_name, tbl)
+        arrow_stream = con.execute(task_config.query).fetch_record_batch(rows_per_batch=chunk_size)
+        write_batches(arrow_stream, destination)
 
 
 def main():
-    """Run aggregation queries for files specified in config."""
+    """Run aggregation and postprocessing queries for files specified in config."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--devrun", action=argparse.BooleanOptionalAction, help="Fast dev run")
 
@@ -53,10 +83,12 @@ def main():
 
     config = OmegaConf.load("config.yaml")
     result_dir = Path(config.output.path)
+    for task_config in config.postprocess.values():
+        postprocess(task_config, config.output)
 
     for tbl, query in config.aggregate.tables.items():
         tbl_src = (Path(config.output.path) / Path(tbl)).with_suffix(config.output.extension)
-        aggregate(query, tbl_src, result_dir, config.aggregate.suffix)
+        aggregate(query, tbl_src, result_dir, config.output.suffix.aggregate)
 
 
 if __name__ == "__main__":
--- src/get_metadata.py
+++ src/get_metadata.py
@@ -1,5 +1,6 @@
 import pathlib
 import re
+import shutil
 from pathlib import Path
 import pyarrow.parquet as pq
 import yaml
@@ -8,11 +9,11 @@ from omegaconf.dictconfig import DictConfig
 
 
 def get_metadata(src_path: pathlib.Path, docs_path: pathlib.Path, config: DictConfig) -> None:
-    """Iterate over files in `src_path` and store metadata in `meta_path`."""
-    aggregate_suffix = config.aggregate.suffix
+    """Iterate over files in `src_path`, store metadata in `meta_path` and store final files."""
     names_replace = config.export.replace_suffix
     extension = config.output.extension
     schema_dict = {}
+    final_file_mapping = {}
     for file in src_path.glob("*.parquet"):
         skip = [f"{x}{extension}" in str(file) for x in names_replace]
         if any(skip):
@@ -22,11 +23,20 @@ def get_metadata(src_path: pathlib.Path, docs_path: pathlib.Path, config: DictCo
 
         file_schema = {name: str(field.type) for name, field in zip(table_schema.names, table_schema, strict=False)}
 
-        file_stem = re.sub(aggregate_suffix, "", file.stem)
+        file_stem = re.sub("|".join(config.output.suffix.values()), "", file.stem)
         schema_dict[file_stem] = file_schema
+        final_file_mapping[file_stem] = file.stem
 
     outfile = docs_path / "schema.yaml"
     outfile.write_text(yaml.dump(schema_dict, default_flow_style=False))
+
+    final_path = Path(*[config.output.path, config.export.final_data])
+    final_path.mkdir(parents=True, exist_ok=True)
+    for target, src in final_file_mapping.items():
+        suffix = config.output.extension
+        src_file = Path(*[config.output.path, src]).with_suffix(suffix)
+        target_file = Path(*[config.output.path, config.export.final_data, target]).with_suffix(suffix)
+        shutil.copy(src_file.with_suffix(suffix), target_file.with_suffix(suffix))
 
 
 def main():
